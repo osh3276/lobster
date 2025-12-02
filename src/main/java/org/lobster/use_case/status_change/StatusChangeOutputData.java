574d74d3b162bc5619eb647a629b43009afe82e3
Isaac-TrtUni <isaac.hong@mail.utoronto.ca>
2025-12-02 00:44:05 -0500

    Status Change Use Case Checkstyle.

--- src/main/java/org/lobster/use_case/status_change/StatusChangeOutputData.java
+++ src/main/java/org/lobster/use_case/status_change/StatusChangeOutputData.java
@@ -2,13 +2,26 @@ package org.lobster.use_case.status_change;
 
 import org.lobster.entity.Flight;
 
+/**
+ * Output data for the Status Change use case.
+ */
 public class StatusChangeOutputData {
     private final Flight updatedFlight;
 
+    /**
+     * Constructs an output data object containing the updated flight.
+     *
+     * @param updatedFlight the flight whose status has just changed
+     */
     public StatusChangeOutputData(Flight updatedFlight) {
         this.updatedFlight = updatedFlight;
     }
 
+    /**
+     * Returns the flight that has been updated.
+     *
+     * @return the updated flight entity
+     */
     public Flight getUpdatedFlight() {
         return updatedFlight;
     }
